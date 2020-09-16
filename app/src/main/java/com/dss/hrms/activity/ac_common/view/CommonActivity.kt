e3ac88e96e3e7febf4409a49c68f6e83360c2154
Towhid <simanto14feb@gmail.com>
2020-09-16 18:11:40 +0600

    DSS-87: Main Lay oput Design for common use

---
+++ app/src/main/java/com/dss/hrms/activity/ac_common/view/CommonActivity.kt
@@ -0,0 +1,12 @@
+package com.dss.hrms.activity.ac_common.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dss.hrms.R
+
+class CommonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+    }
+}
