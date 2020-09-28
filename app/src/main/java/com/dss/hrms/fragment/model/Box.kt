69d5f2914a5fc61d1b038aa605d7dcd5a55d838d
Towhid <simanto14feb@gmail.com>
2020-09-28 10:22:04 +0600

    DSS-450: space added

--- app/src/main/java/com/dss/hrms/fragment/model/Box.kt
+++ app/src/main/java/com/dss/hrms/fragment/model/Box.kt
@@ -1,11 +1,15 @@
 package com.dss.hrms.fragment.model
 
+import android.view.View
+
 class Box(
     private var title: String,
     private var hint: String,
-    private var description: String
+    private var description: String,
+    private var spaceShow: Int=View.GONE
 
 ) {
+
 
     fun getTitle(): String {
         if (title.equals(null))
@@ -21,5 +25,8 @@ class Box(
         if (description.equals(null))
             return ""
         return description
+    }
+    fun getSpaceShow(): Int {
+        return spaceShow
     }
 }
